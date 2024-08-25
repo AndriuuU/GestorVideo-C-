8883e51c5fd47ea0c7a5a96a8fe3d5960a558058
AndriuuU <66785221+AndriuuU@users.noreply.github.com>
2024-08-25 18:59:25 +0200

    Primer comic

--- VideoManager/obj/Debug/net8.0/VideoManager.AssemblyInfo.cs
+++ VideoManager/obj/Debug/net8.0/VideoManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd564ce45bfd84f4a6a3dd61b307ff020f19245e")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

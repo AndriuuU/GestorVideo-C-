ab516fe0322f9bc98e1150039f9a000ae25577ee
AndriuuU <66785221+AndriuuU@users.noreply.github.com>
2024-08-28 18:01:59 +0200

    .

--- VideoManager/Program.cs
+++ VideoManager/Program.cs
@@ -17,7 +17,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var initializer = scope.ServiceProvider.GetRequiredService<DatabaseInitializer>();
-    //await initializer.InitializeAsync();  // Inicializar la base de datos y datos de prueba
+    await initializer.InitializeAsync();  // Inicializar la base de datos y datos de prueba
 }
 
 // Configure the HTTP request pipeline.
